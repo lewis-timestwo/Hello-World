c78a454e106f177a2908c146d2cb77aa762a773d
Lewis <lewisluia101@gmail.com>
2023-02-17 15:17:53 -0500

    Pokemon Sprites

--- script.js
+++ script.js
@@ -9,6 +9,21 @@ pokeForm.addEventListener("submit", (event) =>{
       event.preventDefault()
       result = document.querySelector('input').value
       page = 1;
-      //searchPokemon(result)
+      searchPokemon(result)
       resultsHeader.textContent = "Result for: " + result
 })
+
+async function  searchPokemon(event){
+    let response = await fetch (`https://pokeapi.co/api/v2/pokemon/${event}/`)
+    let responseData = await response.json()
+    console.log(responseData)
+    displayResults(responseData)
+}
+
+function displayResults(event){
+    pokemonGrid.innerHTML += `
+    <div class="pokemon-card">
+    <img class="pokemon-image" alt="sprite for ${event}" src=${event.sprites.front_default}>
+    </div>
+    `
+}
